cabfe98dbb857d6878ae96afe3a2e5745ec0dfc2
Gong Yi <yi.gong@microfocus.com>
2020-06-14 21:15:50 +0800

    Cleanup the test

--- propertysource-reload-spring-boot-starter/src/test/java/io/github/topikachu/properties/reload/AppTest.java
+++ propertysource-reload-spring-boot-starter/src/test/java/io/github/topikachu/properties/reload/AppTest.java
@@ -8,19 +8,25 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.event.EventListener;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.times;
@@ -29,12 +35,17 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT, classes = App.class)
-@AutoConfigureWebTestClient
-
+@Import({ AppTest.LockConfiguration.class, AppTest.PropertySourceReloadEventListener.class })
 public class AppTest {
 
 	@Autowired
 	private TestRestTemplate testRestTemplate;
+
+	@Autowired
+	private Lock lock;
+
+	@Autowired
+	private Condition condition;
 
 	@SpyBean
 	private PropertySourceReloadEventListener propertySourceReloadEventListener;
@@ -43,51 +54,82 @@ public class AppTest {
 
 	@BeforeClass
 	static public void initConfigFile() throws IOException {
-
 		FileUtils.touch(file);
+		writePropertiesFileWithValue("World");
+	}
+
+	private static void writePropertiesFileWithValue(String word) throws IOException {
 		Properties properties = new Properties();
-		properties.setProperty("bean.name", "World");
+		properties.setProperty("bean.name", word);
 		try (FileOutputStream fos = new FileOutputStream(file)) {
 			properties.store(fos, "gen by test");
 			fos.flush();
 		}
 	}
 
-	@Test
+	@Test(timeout = 10000)
 	@SneakyThrows
 	public void testApp() {
-		{
-			ResponseEntity<String> response = this.testRestTemplate.getForEntity("/greeting", String.class);
-			assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-			assertThat(response.getBody()).isEqualTo("Hello World");
-		}
-		Properties properties = new Properties();
-		properties.setProperty("bean.name", "World2");
-		try (FileOutputStream fos = new FileOutputStream(file)) {
-			properties.store(fos, "gen by test");
-			fos.flush();
-		}
-		Thread.sleep(6000);
-		ArgumentCaptor<PropertySourceReloadEvent> captor = ArgumentCaptor.forClass(PropertySourceReloadEvent.class);
 
+		assertGreetingApiWithContent("Hello World");
+
+		writePropertiesFileWithValue("World2");
+
+		lock.lock();
+		condition.await();
+		lock.unlock();
+
+		assertPropertySourceReloadEvent();
+
+		assertGreetingApiWithContent("Hello World2");
+
+	}
+
+	private void assertPropertySourceReloadEvent() {
+		ArgumentCaptor<PropertySourceReloadEvent> captor = ArgumentCaptor.forClass(PropertySourceReloadEvent.class);
 		verify(propertySourceReloadEventListener, times(1)).onRefresh(captor.capture());
 		PropertySourceReloadEvent captorPropertySourceReloadEvent = captor.getValue();
 		assertThat(captorPropertySourceReloadEvent.getKeys()).containsOnly("bean.name");
 		assertThat(captorPropertySourceReloadEvent.getFileEvent())
 				.isEqualTo(PropertySourceReloadEvent.FileEvent.CHANGE);
 		assertThat(captorPropertySourceReloadEvent.getFile()).hasName("app.properties");
-		{
-			ResponseEntity<String> response = this.testRestTemplate.getForEntity("/greeting", String.class);
-			assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-			assertThat(response.getBody()).isEqualTo("Hello World2");
-		}
 	}
 
+	private void assertGreetingApiWithContent(String content) {
+		ResponseEntity<String> response = this.testRestTemplate.getForEntity("/greeting", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).isEqualTo(content);
+	}
+
+	@Configuration
+	static class LockConfiguration {
+
+		@Bean
+		public Lock lock() {
+			return new ReentrantLock();
+		}
+
+		@Bean
+		public Condition condition(Lock lock) {
+			return lock.newCondition();
+		}
+
+	}
+
+	@Component
 	public static class PropertySourceReloadEventListener {
+
+		@Autowired
+		private Lock lock;
+
+		@Autowired
+		private Condition condition;
 
 		@EventListener(PropertySourceReloadEvent.class)
 		public void onRefresh(PropertySourceReloadEvent event) {
-
+			lock.lock();
+			condition.signalAll();
+			lock.unlock();
 		}
 
 	}
